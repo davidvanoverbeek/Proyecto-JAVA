0338e2194eb6326b12577b67335e7600d7119f58
David Antonio van Overbeek Crusells <davidvanoverbeekcrusells@gmail.com>
2025-08-05 18:36:44 +0200

    Restructuracion clase Venta

--- src/Venta.java
+++ src/Venta.java
@@ -2,5 +2,39 @@ import java.util.ArrayList;
 
 public class Venta {
 
+    private Cliente cliente;
+    private ArrayList<Producto> lineasDeVenta;
+    private String fecha;
 
+    public Venta(Cliente cliente, String fecha) {
+        this.cliente = cliente;
+        this.fecha = fecha;
+        this.lineasDeVenta = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        lineasDeVenta.add(producto);
+    }
+
+    public double calcuclarTotal() {
+        double total = 0;
+        for (Producto producto : lineasDeVenta) {
+            total += producto.getPrecio();
+        }
+        return total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta {" +
+                "cliente: " + cliente.getNombre() +
+                ", fecha: " + fecha + '\'' +
+                ", total: " + calcuclarTotal() +
+                ", producto: " + lineasDeVenta +
+                '}';
+    }
 }
