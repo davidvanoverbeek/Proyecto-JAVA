86fe3b2be09b638218ab9b6b1fd96fabe5c86c52
David Antonio van Overbeek Crusells <davidvanoverbeekcrusells@gmail.com>
2025-08-05 18:35:35 +0200

    Restructuracion calse Producto

--- src/Producto.java
+++ src/Producto.java
@@ -1,4 +1,33 @@
 public class Producto {
 
+    private String articulo;
+    private String tamaño;
+    private Double precio;
 
+    public Producto(String articulo, String tamaño, Double precio) {
+        this.articulo = articulo;
+        this.tamaño = tamaño;
+        this.precio = precio;
+    }
+
+    public String getArticulo() {
+        return articulo;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto {" +
+                "articulo: " + articulo + '\'' +
+                ", tamaño: " + tamaño + '\'' +
+                ", precio: " + precio +
+                '}';
+    }
 }
